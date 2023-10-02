e9ae83d4283ec6db54b58095513103b2cb3823a9
Shortsighted <30327719+Shortsighted@users.noreply.github.com>
2023-10-02 22:33:12 +0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,31 @@
+function createGraph(howManyD6, howManyTimes) {
+    const totalArray = []
+    const countArray = []
+    const array = []
+
+    for(let i=0; i<howManyD6*6+1; i++){
+        countArray.push(0)
+    }
+
+    for(let i=0; i<howManyTimes; i++){
+        let sum = 0
+        for(let k=0; k<howManyD6; k++){
+            sum += Math.floor(Math.random()*6)+1
+        }
+        totalArray.push(sum)
+        countArray[sum] += 1
+    }
+
+    for(let item of countArray){
+        const percentage = item/(howManyTimes/100)
+        let string = ""
+
+        for(let i=percentage; i>0; i--){
+            string += "-"
+        }
+        string += `   ${percentage}`
+        array.push(string)
+    }
+
+    console.log(array)
+}
